09044f65a0aa2eda5555824be379074abd3d8f23
Elizabeth Boswell <2398840B@student.gla.ac.uk>
2020-03-03 18:39:34 +0000

    added very basic about, contact and base.html

--- FreeFoodFinder/settings.py
+++ FreeFoodFinder/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'finder',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'FreeFoodFinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#If true, users can register
+REGISTRATION_OPEN = True
+
+#if true, the user will be automatically logged in after registering
+REGISTRATION_AUTO_LOGIN = True
+
+#the page users are directed to if they are not logged in
--- finder/views.py
+++ finder/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+
+def about(request):
+    return render(request, 'finder/about.html')
+
+def contact(request):
+    return render(request, 'finder/contact.html')
