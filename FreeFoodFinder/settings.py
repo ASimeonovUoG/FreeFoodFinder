e94bb464fcafa80d8baf1c4e8ef2eee374b01233
Alexander Simeonov <2390362S@student.gla.ac.uk>
2020-04-01 20:45:26 +0100

    STATIC_ROOT for PA setup.

--- FreeFoodFinder/settings.py
+++ FreeFoodFinder/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #If true, users can register
 REGISTRATION_OPEN = True
