61b6c8b7573b94edcc326a6a01ca1af22c42cdff
Alexander Simeonov <2390362S@student.gla.ac.uk>
2020-04-01 21:27:17 +0100

    Fix STATIC_ROOT so localhost can runserver.

--- FreeFoodFinder/settings.py
+++ FreeFoodFinder/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 #If true, users can register
 REGISTRATION_OPEN = True
