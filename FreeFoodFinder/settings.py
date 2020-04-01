c2b467f3fc9b77e73b10b26bd88e8891f599af93
Alexander Simeonov <2390362S@student.gla.ac.uk>
2020-04-01 20:35:04 +0100

    Setting up allowed hosts.

--- FreeFoodFinder/settings.py
+++ FreeFoodFinder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'xjf=vnfay1-m+dw-cm$i4l04mipyc*ist_dq0b3=jlsp$jt^=!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexanderuog.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
